356525b02665a0c232aadc5ccf6cfe3b231be84d
arthur <arthur.broudoux@gmail.com>
2024-12-05 16:57:53 +0100

    feat: option to create a new branch Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- branch.sh
+++ branch.sh
@@ -58,34 +58,33 @@ function branch() {
         return 1
     fi
 
-    if [[ "$selected_option" == "Create a new branch" ]]; then
-        new_branch_name=$(fzf --prompt "Enter the new branch name: " --height 10%) || { echo "Action canceled."; return; }
+    echo "You selected branch: $selected_option"
 
-        if [[ -n "$new_branch_name" ]]; then
-            git checkout -b "$new_branch_name" && echo "New branch '$new_branch_name' created."
-        else
+    action=$(echo -e "Copy\nCheckout\nNew\nMerge\nDelete\nCancel" | fzf --prompt "Choose an action: " --height 10% --pointer ">") || { echo "Action canceled."; return; }
+
+    if [[ -z "$action" ]]; then
+        echo "No action selected."
+        return 1
+    fi
+
+    if [[ "$action" == "Copy" ]]; then
+        echo -n "$selected_option" | pbcopy && git push && echo "Branch '$selected_option' copied to clipboard."
+    elif [[ "$action" == "Checkout" ]]; then
+        git checkout "$selected_option" && echo "Branch '$selected_option' checked out."
+    elif [[ "$action" == "New" ]]; then
+        read "name?Enter a name for your new branch: "
+
+        if [[ -z "$name" ]]; then
             echo "No branch name provided. Action canceled."
-        fi
-    else
-        echo "You selected branch: $selected_option"
-
-        action=$(echo -e "Copy\nCheckout\nMerge\nDelete\nCancel" | fzf --prompt "Choose an action: " --height 10% --pointer ">") || { echo "Action canceled."; return; }
-
-        if [[ -z "$action" ]]; then
-            echo "No action selected."
             return 1
         fi
 
-        if [[ "$action" == "Copy" ]]; then
-            echo -n "$selected_option" | pbcopy && git push && echo "Branch '$selected_option' copied to clipboard."
-        elif [[ "$action" == "Checkout" ]]; then
-            git checkout "$selected_option" && echo "Branch '$selected_option' checked out."
-        elif [[ "$action" == "Merge" ]]; then
-            git merge "$selected_option" && echo "Branch '$selected_option' merged."
-        elif [[ "$action" == "Delete" ]]; then
-            git branch -D "$selected_option" && echo "Branch '$selected_option' deleted."
-        else
-            echo "Action canceled."
-        fi
+        git checkout -b "$name" && echo "Branch '$name' based on '$selected_option' created."
+    elif [[ "$action" == "Merge" ]]; then
+        git merge "$selected_option" && echo "Branch '$selected_option' merged."
+    elif [[ "$action" == "Delete" ]]; then
+        git branch -D "$selected_option" && echo "Branch '$selected_option' deleted."
+    else
+        echo "Action canceled."
     fi
 }
