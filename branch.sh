05cdb278826cde9b4f063cf03323a2f6e766d2f7
arthur <arthur.broudoux@gmail.com>
2024-12-05 10:30:03 +0100

    feat: create -v / -h flags Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- branch.sh
+++ branch.sh
@@ -13,10 +13,28 @@ function branch() {
         return 1
     fi
 
+    if [[ $# -eq 0 ]]; then
+        echo "You must provide an option"
+        ecjo "Use --help or -h for usage information."
+        return
+    fi
+
     while [[ $# -gt 0 ]]; do
         case "$1" in
             --list|-l)
                 echo $branches
+                return
+                ;;
+            --version|-v)
+                version=$(jq -r '.version' ./package.json) 
+                echo "$version"
+                return
+                ;;
+            --help|-h)
+                echo "Usage: branch [options]"
+                echo "Options:"
+                echo "  --list, -l            List all branches"
+                echo "  --help, -h            Show this help message"
                 return
                 ;;
         esac
