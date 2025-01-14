64441117dfbb9ce40cff279abff1522ee2ed934c
arthur <arthur.broudoux@gmail.com>
2025-01-14 10:06:28 +0100

    chore: update PrintHelpManual method Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,8 +8,6 @@ import (
 
 func PrintHelpManual() {
 	fmt.Println("Usage: branch [options]")
-	fmt.Printf("  %-20s %s\n", "branch [run | -r]", "Start the interactive branch selection")
-	fmt.Printf("  %-20s %s\n", "branch [--list | -l]", "List all branches")
 	fmt.Printf("  %-20s %s\n", "branch [--help | -h]", "Show this help message")
 }
 
@@ -17,7 +15,7 @@ func GetLatestRelease() (string, error) {
 	url := "https://api.github.com/repos/abroudoux/branch/releases/latest"
 	res, err := http.Get(url)
 	if err != nil {
-		return "", fmt.Errorf("error while fetching latest release: %v", err)
+		return "", err
 	}
 
 	latestVersion := res.Header.Get("tag_name")
@@ -31,7 +29,7 @@ func FlagMode() error {
 	case "-v", "--version":
 		latestVersion, err := GetLatestRelease()
 		if err != nil {
-			return fmt.Errorf("error getting latest version: %v", err)
+			return err
 		}
 
 		fmt.Printf("Latest version: %s\n", latestVersion)
