2325aa6f1a2cf676d3424946093231b7617a6b23
Arthur Broudoux <arthur.broudoux@ouest-france.fr>
2025-02-13 14:42:27 +0100

    fix: change branch name during remote deletion

--- internal/actions/delete.go
+++ internal/actions/delete.go
@@ -47,7 +47,7 @@ func delete(repo git.Repository, branch branches.BranchDetails) error {
 	}
 
 	if confirmRemoteDeletion {
-		err = repo.DeleteBranch("/refs/remotes/origin/" + branch.Name)
+		err = repo.DeleteBranch("refs/remotes/origin/" + branch.Name)
 		if err != nil {
 			return fmt.Errorf("failed to delete remote branch: %w", err)
 		}
